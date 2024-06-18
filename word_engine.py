baf2c181ec13d2dcbd4e25cefb8773d3468cb77f
Liam S. <liam.alan.smth@gmail.com>
2024-06-18 20:21:19 +0100

    Create WordEngine class for fetching lorem ipsum text from API.

---
+++ word_engine.py
@@ -0,0 +1,24 @@
+import requests
+
+
+class WordEngine:
+    def __init__(self, num_sentences):
+        self.words_API_endpoint = "https://baconipsum.com/api/"
+        self.words_API_params = {"type": "all-meat",
+                                 "sentences": 40}
+        self.sentences = self.fetch_words(num_sentences)
+
+    def fetch_words(self, num_sentences):
+        self.words_API_params["sentences"] = num_sentences
+        response = requests.get(self.words_API_endpoint,
+                                self.words_API_params)
+        response.raise_for_status()
+        json = response.json()
+        return json[0]
+
+
+if __name__ == "__main__":
+    engine = WordEngine(70)
+
+
+
